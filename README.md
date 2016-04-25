46c0ec433e677189608b2c9d0541ffda9cd26f8e
Emily Trawick <trawiced@dukes.jmu.edu>
2016-04-24 20:31:52 -0400

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# math_sec3
+#ISAT 252 Section 3 TDD Math Program
+
+This is a simple program to demonstrate how to do test-driven development with JavaScript, Node, and Jasmine.
---
+++ spec/math_spec.js
@@ -0,0 +1,7 @@
+var math = require('../math')
+
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(add(3,5)).toBe(8);
+  });
+});
